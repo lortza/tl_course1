41297ced0930166672f64e885c9c9d4645e03030
anne <richardson.ae@gmail.com>
2015-09-22 21:07:07 -0500

    object model exercises

---
+++ lesson2/oop_book.rb
@@ -0,0 +1,19 @@
+# Book Exercises
+
+# http://www.gotealeaf.com/books/oo_ruby/read/the_object_model#modules
+
+#1 How do we create an object in Ruby? Give an example of the creation of an object.
+
+class Owner
+end#class
+
+o = Owner.new
+
+
+#2 What is a module? What is its purpose? How do we use them with our classes? Create a module for the class you created in exercise 1 and include it properly.
+
+module Speak
+  def speak(sound)
+    puts "#{sound}"
+  end#speak
+end#module
