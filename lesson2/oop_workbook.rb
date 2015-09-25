212d3ec62e5a3ff1b7eb3aab3f006c27b32ec3ba
anne <richardson.ae@gmail.com>
2015-09-25 15:50:52 -0500

    Q1E1

---
+++ lesson2/oop_workbook.rb
@@ -0,0 +1,10 @@
+# http://www.gotealeaf.com/books/oo_workbook/read/easy_quiz_1
+
+#1 Which of the following are objects in Ruby? If they are objects, how can you find out what class they belong to?
+
+true # => TrueClass
+"hello" # => String
+[1, 2, 3, "happy days"] # => Array
+142 # => FixNum
+
+# you can find out the class of any object by entering it + .class in irb
