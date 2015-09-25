b69719c318bdf88af8eec4749c392fe513068c41
anne <richardson.ae@gmail.com>
2015-09-25 16:38:50 -0500

    Q2E2

--- lesson2/oop_workbook_easy.rb
+++ lesson2/oop_workbook_easy.rb
@@ -172,3 +172,25 @@ oracle = Oracle.new
 oracle.predict_the_future # => a random sample from the array in the choices method
 
 
+# 2 We have an Oracle class and a RoadTrip class that inherits from the Oracle class.
+
+class Oracle
+  def predict_the_future
+    "You will " + choices.sample
+  end
+
+  def choices
+    ["eat a nice lunch", "take a nap soon", "stay at work late"]
+  end
+end
+
+class RoadTrip < Oracle
+  def choices
+    ["visit Vegas", "fly to Fiji", "romp in Rome"]
+  end
+end
+
+#What is the result of the following:
+trip = RoadTrip.new
+p trip.predict_the_future # => selects from the choices within RoadTrip
+
