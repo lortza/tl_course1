10a8e37b91ced9638c5b2d33768d1086f16b62a2
anne <richardson.ae@gmail.com>
2015-09-25 15:55:39 -0500

    Q1E2

--- lesson2/oop_workbook.rb
+++ lesson2/oop_workbook.rb
@@ -8,3 +8,30 @@ true # => TrueClass
 142 # => FixNum
 
 # you can find out the class of any object by entering it + .class in irb
+
+#If we have a Car class and a Truck class and we want to be able to go_fast, how can we add the ability for them to go_fast using the module Speed. How can you check if your Car or Truck can now go fast?
+
+module Speed
+  def go_fast
+    puts "I am a #{self.class} and going super fast!"
+  end
+end
+
+class Car
+  include Speed # insert Speed into this class
+  def go_slow
+    puts "I am safe and driving slow."
+  end
+end
+
+class Truck
+  include Speed # insert Speed into this class
+  def go_very_slow
+    puts "I am a heavy truck and like going very slow."
+  end
+end
+
+c = Car.new
+t = Truck.new
+c.go_fast # => I am a Car and going super fast!
+t.go_fast # => I am a Car and going super fast!
