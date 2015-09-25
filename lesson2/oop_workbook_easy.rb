9538f3b142a43fbcc11c56ca478b5aa7313a95c5
anne <richardson.ae@gmail.com>
2015-09-25 16:52:13 -0500

    Q2E5

--- lesson2/oop_workbook_easy.rb
+++ lesson2/oop_workbook_easy.rb
@@ -250,3 +250,12 @@ end
 
 b = BeesWax.new("waxy wax")
 b.describe_type # => I am a waxy wax of Bees Wax
+
+
+#5 There are a number of variables listed below. What are the different types and how do you know which is which?
+
+excited_dog = "excited dog" # => regualar local variable
+@excited_dog = "excited dog" # => instance variable
+@@excited_dog = "excited dog" # => class variable
+
+
