86278c91bd1f9a23ac6d575e35854078e1680b46
anne <richardson.ae@gmail.com>
2015-09-25 16:41:34 -0500

    Q2E3

--- lesson2/oop_workbook_easy.rb
+++ lesson2/oop_workbook_easy.rb
@@ -194,3 +194,23 @@ end
 trip = RoadTrip.new
 p trip.predict_the_future # => selects from the choices within RoadTrip
 
+#3 How do you find where Ruby will look for a method when that method is called? How can you find an object's ancestors? What is the lookup chain for Orange and HotSauce?
+
+module Taste
+  def flavor(flavor)
+    puts "#{flavor}"
+  end
+end
+
+class Orange
+  include Taste
+end
+
+class HotSauce
+  include Taste
+end
+
+#answer: 
+p Orange.ancestors # => [Orange, Taste, Object, Kernel, BasicObject]
+p HotSauce.ancestors # => [HotSauce, Taste, Object, Kernel, BasicObject]
+
