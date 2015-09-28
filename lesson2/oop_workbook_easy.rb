027d2cb7c5c61be426c07655b6ad99d25bfffba4
anne <richardson.ae@gmail.com>
2015-09-28 10:37:52 -0500

    Q3E6

--- lesson2/oop_workbook_easy.rb
+++ lesson2/oop_workbook_easy.rb
@@ -462,3 +462,41 @@ tv.model # => "printing out the word model"
 
 Television.manufacturer # => "this is the self.manufacturer method"
 Television.model # => undefined method b/c this is a not a class-level method
+
+
+
+#6 If we have a class such as the one below, In the make_one_year_older method we have used self. What is another way we could write this method so we don't have to use the self prefix.
+
+# class Cat
+#   attr_accessor :type, :age
+
+#   def initialize(type)
+#     @type = type
+#     @age  = 0
+#   end
+
+#   def make_one_year_older
+#     self.age += 1
+#   end
+# end
+
+
+#solution
+
+class Cat
+  attr_accessor :type, :age
+
+  def initialize(type)
+    @type = type
+    @age  = 0
+  end
+
+  def make_one_year_older
+    @age += 1 # remove self and just increment the age
+  end
+end #Cat
+
+c = Cat.new("tabby")
+p c.make_one_year_older
+
+
