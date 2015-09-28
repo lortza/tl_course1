14345a1c6fd6646de3f95bda05752f1667bf54b1
anne <richardson.ae@gmail.com>
2015-09-28 11:02:53 -0500

    Q1E4

--- lesson2/oop_workbook_int.rb
+++ lesson2/oop_workbook_int.rb
@@ -64,3 +64,40 @@ end
 
 #answer: it's not a very rails-friendly fix. 
 # book answer: Nothing incorrect syntactically. However, you are altering the public interfaces of the class. In other words, you are now allowing clients of the class to change the quantity directly (calling the accessor with the instance.quantity = <new value> notation) rather than by going through the update_quantity method. It means that the protections built into the update_quantity method can be circumvented and potentially pose problems down the line.
+
+
+# Let's practice creating an object hierarchy.
+
+# Create a class called Greeting with a single method called greet that takes a string argument and prints that argument to the terminal.
+
+# 4 Now create two other classes that are derived from Greeting: one called Hello and one called Goodbye. The Hello class should have a hi method that takes no arguments and prints "Hello". The Goodbye class should have a bye method to say "Goodbye". Make use of the Greeting class greet method when implementing the Hello and Goodbye classes - do not use any puts in the Hello or Goodbye classes.
+
+class Greeting
+  def greet(message)
+    puts message
+  end #greet
+end #Greeting
+
+
+class Hello < Greeting
+  def say_hi
+     greet("hi (this is Hello's say_hi method)")
+  end #say_hi
+end #Hello
+
+class Goodbye < Greeting
+  def say_bye
+    greet("bye (this is Goodbye's say_bye method)")
+  end #say_bye
+end #Goodbye
+
+g = Greeting.new
+p g.greet("sample tester class greeting")
+
+h = Hello.new
+p h.say_hi
+
+b = Goodbye.new
+p b.say_bye
+
+
