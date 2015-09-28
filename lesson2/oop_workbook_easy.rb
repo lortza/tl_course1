5ee14238dacf82ada952874f0d7f28e47bc43c76
anne <richardson.ae@gmail.com>
2015-09-28 09:54:50 -0500

    Q3E4

--- lesson2/oop_workbook_easy.rb
+++ lesson2/oop_workbook_easy.rb
@@ -423,4 +423,25 @@ p cat1 # => #<AngryCat:0x007fc8918c8730 @age=2, @name="Zorro">
 p cat2 # => #<AngryCat:0x007fc8918c86e0 @age=3, @name="DaBeef">
 
 
+#4 Given the class below, if we created a new instance of the class and then called to_s on that instance we would get something like "#<Cat:0x007ff39b356d30>". How could we go about changing the to_s output on this method to look like this: I am a tabby cat? (this is assuming that "tabby" is the type we passed in during initialization).
+
+class Cat
+  def initialize(type)
+    @type = type
+  end
+end
+
+#solution
+class Cat
+  def initialize(type)
+    @type = type
+  end
+  def to_s
+    "i am a #{@type} cat."
+  end
+end
+
+c = Cat.new("tabby")
+p c.to_s
+
 
