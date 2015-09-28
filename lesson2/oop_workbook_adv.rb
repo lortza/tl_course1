f9e367633f806a68f59db3ebb46ed07d85ccc719
anne <richardson.ae@gmail.com>
2015-09-28 11:58:39 -0500

    Q1E4

--- lesson2/oop_workbook_adv.rb
+++ lesson2/oop_workbook_adv.rb
@@ -205,3 +205,82 @@ class Motorboat < Vehicle
 
 end #Motorboat
 
+
+#4 The designers of the vehicle management system now want to make an adjustment for how the range of vehicles is calculated. For the seaborne vehicles, due to prevailing ocean currents, they want to add an additional 10km of range even if the vehicle is out of fuel. Alter the code related to vehicles so that the range for autos and motorcycles is still calculated as before, but for catamarans and motorboats, the range method will return an additional 10km.
+
+
+module WheeledVehicle
+  def initialize(tire_array)
+    @tires = tire_array
+  end #initialize
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end #tire_pressure
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end #inflate_tire
+end #WheeledVehicle
+
+
+module FloatingVehicle
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    # ... code omitted ...
+  end #initialize
+
+  def range
+    range_by_using_fuel = super
+    return range_by_using_fuel + 10
+  end
+end #FloatingVehicle
+
+
+class Vehicle
+  attr_accessor :speed, :heading
+
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    @fuel_efficiency = km_traveled_per_liter
+    @fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class Auto < Vehicle
+  include WheeledVehicle
+
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end #initialize
+end #Auto
+
+class Motorcycle < Vehicle
+  include WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures along with 
+    super([20,20], 80, 8.0)
+  end #initialize
+end #Motorcycle
+
+class Catamaran < Vehicle
+  include FloatingVehicle
+  attr_accessor :propeller_count, :hull_count, :speed, :heading, :range
+end #Catamaran
+
+class Motorboat < Vehicle
+  include FloatingVehicle
+  attr_accessor :speed, :heading, :range
+
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    @num_propellers = 1
+    @num_hulls = 1
+    @km_traveled_per_liter = km_traveled_per_liter
+    @liters_of_fuel_capacity = liters_of_fuel_capacity
+  end #initialize
+
+end #Motorboat
+
