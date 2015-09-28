c6f71d56a18fa13771939839fb562f87b2ff6f06
anne <richardson.ae@gmail.com>
2015-09-28 10:48:01 -0500

    Q1E2

--- lesson2/oop_workbook_int.rb
+++ lesson2/oop_workbook_int.rb
@@ -21,3 +21,22 @@ end
 # Ben is right because he made "balance" available via the attr_reader. 
 
 
+#2 Alyssa created the following code to keep track of items for a shopping cart application she's writing. Alan looked at the code and spotted a mistake. "This will fail when update_quantity is called", he says. Can you spot the mistake and how to address it?
+
+class InvoiceEntry
+  attr_reader :quantity, :product_name
+
+  def initialize(product_name, number_purchased)
+    @quantity = number_purchased
+    @product_name = product_name
+  end
+
+  def update_quantity(updated_count)
+    # prevent negative quantities from being set
+    quantity = updated_count if updated_count >= 0
+  end
+
+end #InvoiceEntry
+
+#answer: he's used attr_reader, which does not allow for writing to that variable
+
