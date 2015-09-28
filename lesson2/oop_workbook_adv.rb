5f79916b246f8c0d19c509af17ceac82d77a68e3
anne <richardson.ae@gmail.com>
2015-09-28 11:48:17 -0500

    Q1E2

--- lesson2/oop_workbook_adv.rb
+++ lesson2/oop_workbook_adv.rb
@@ -25,3 +25,110 @@ end #SecurityLogger
  # Hint: assume that you can modify the initialize method in SecretFile to have an instance of SecurityLogger be passed in as an additional argument.
 
  #solution: i don't really get it and the book solution doesn't make sense to me. 
+
+
+
+ #2 Ben and Alyssa are working on a vehicle management system. So far, they have created classes called Auto and Motorcycle to represent automobiles and motorcycles. After having noticed common information and calculations they were performing for each type of vehicle, they decided to break out the commonality into a separate class called WheeledVehicle. This is what their code looks like so far:
+
+class WheeledVehicle
+  attr_accessor :speed, :heading
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    @fuel_efficiency = km_traveled_per_liter
+    @fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures along with 
+    super([20,20], 80, 8.0)
+  end
+end
+
+# Now Alan has asked them to incorporate a new type of vehicle into their system - a Catamaran defined as follows:
+
+class Catamaran
+  attr_accessor :propeller_count, :hull_count, :speed, :heading, :range
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    # ... code omitted ...
+  end
+end
+
+# This new class does not fit well with the object hierarchy defined so far. Catamarans don't have tires. But we still want common code to track fuel efficiency and range. Modify the class definitions and move code into a Module, as necessary, to share code among the Catamaran and the wheeled vehicles.
+
+#solution:
+module WheeledVehicle
+  def initialize(tire_array)
+    @tires = tire_array
+  end #initialize
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end #tire_pressure
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end #inflate_tire
+end #WheeledVehicle
+
+
+class Vehicle
+  attr_accessor :speed, :heading
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @fuel_efficiency = km_traveled_per_liter
+    @fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class Auto < Vehicle
+  include WheeledVehicle
+
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end #initialize
+end #Auto
+
+class Motorcycle < Vehicle
+  include WheeledVehicle
+
+  def initialize
+    # 2 tires are various tire pressures along with 
+    super([20,20], 80, 8.0)
+  end #initialize
+end #Motorcycle
+
+class Catamaran < Vehicle
+  attr_accessor :propeller_count, :hull_count, :speed, :heading, :range
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    # ... code omitted ...
+  end #initialize
+end #Catamaran
