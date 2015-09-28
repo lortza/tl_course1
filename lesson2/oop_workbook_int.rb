2e0d758947e8fbd057b2284b97b659a724144272
anne <richardson.ae@gmail.com>
2015-09-28 10:53:30 -0500

    Q1E3

--- lesson2/oop_workbook_int.rb
+++ lesson2/oop_workbook_int.rb
@@ -1,3 +1,6 @@
+#Intermediate questions
+#http://www.gotealeaf.com/books/oo_workbook/read/intermediate_quiz_1
+
 #1 Ben asked Alyssa to code review the following code:
 
 class BankAccount
@@ -40,3 +43,24 @@ end #InvoiceEntry
 
 #answer: he's used attr_reader, which does not allow for writing to that variable
 
+
+#3 In the last question Alyssa showed Alan this code which keeps track of items for a shopping cart application:
+
+class InvoiceEntry
+  attr_reader :quantity, :product_name
+
+  def initialize(product_name, number_purchased)
+    @quantity = number_purchased
+    @product_name = product_name
+  end
+
+  def update_quantity(updated_count)
+    quantity = updated_count if updated_count >= 0
+  end
+end
+# Alan noticed that this will fail when update_quantity is called. Since quantity is an instance variable, it must be accessed with the @quantity notation when setting it. One way to fix this to change attr_reader to attr_accessor.
+
+# Is there anything wrong with fixing it this way?
+
+#answer: it's not a very rails-friendly fix. 
+# book answer: Nothing incorrect syntactically. However, you are altering the public interfaces of the class. In other words, you are now allowing clients of the class to change the quantity directly (calling the accessor with the instance.quantity = <new value> notation) rather than by going through the update_quantity method. It means that the protections built into the update_quantity method can be circumvented and potentially pose problems down the line.
