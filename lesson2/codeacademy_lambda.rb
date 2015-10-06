d3f746eee8456cfb4e1cfc22f91309ec12fbdcb1
anne <richardson.ae@gmail.com>
2015-10-06 16:50:39 -0500

    second example, searches for names before M

--- lesson2/codeacademy_lambda.rb
+++ lesson2/codeacademy_lambda.rb
@@ -1,4 +1,4 @@
-#make a lambda to check if an item in the array is a symbol and then push the symbols into an array called "symbols"
+#1 make a lambda to check if an item in the array is a symbol and then push the symbols into an array called "symbols"
 
 my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
 
@@ -7,3 +7,21 @@ symbol_filter = lambda{|x| x.is_a? Symbol}
 symbols = my_array.select(&symbol_filter)
 
 p symbols
+
+
+
+# 2 create a lambda that selects crew members with names that come before M
+crew = {
+  captain: "Picard",
+  first_officer: "Riker",
+  lt_cdr: "Data",
+  lt: "Worf",
+  ensign: "Ro",
+  counselor: "Troi",
+  chief_engineer: "LaForge",
+  doctor: "Crusher"
+}
+
+first_half = lambda{|k, v| v < "M"}
+a_to_m = crew.select(&first_half)
+p a_to_m
